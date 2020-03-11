376158a03cc025c6ad4fd31b76e06c541bed1b23
Rocky Le <rockyhoangle@gmail.com>
2020-03-11 10:50:10 -0400

    Add gameplay gui
    Import art assets into the project. In the gameplay class create gui elements for the title image and gameplay canvas which will be added to the JFrame object  through Main class.

--- src/com/phantomxstudios/snake/Gameplay.java
+++ src/com/phantomxstudios/snake/Gameplay.java
@@ -1,15 +1,32 @@
 package com.phantomxstudios.snake;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class Gameplay extends JPanel {
+	private ImageIcon titleImage;
+	
 	public Gameplay() {
 		
 	}
 	
 	public void paint(Graphics g) {
+		
+		
+		// Draw the title image
+		titleImage = new ImageIcon("snaketitle.jpg");
+		titleImage.paintIcon(this, g, 21, 11);
+		
+		// Draw border for gameplay
+		g.setColor(Color.WHITE);
+		g.drawRect(24, 74, 841, 577);
+		
+		// Draw background
+		g.setColor(Color.black);
+		g.fillRect(25, 75, 840, 575);
 		
 	}
 
