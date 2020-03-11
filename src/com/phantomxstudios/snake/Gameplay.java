155a187e37d82ccb6b60e9b0ea7d0a7b718df611
Rocky Le <rockyhoangle@gmail.com>
2020-03-11 10:34:23 -0400

    Create Gameplay class
    After setting up the Main class create Gameplay class which will encompass the the actual game and gameplay. Then add the gameplay class to the JFrame object.

---
+++ src/com/phantomxstudios/snake/Gameplay.java
@@ -0,0 +1,16 @@
+package com.phantomxstudios.snake;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class Gameplay extends JPanel {
+	public Gameplay() {
+		
+	}
+	
+	public void paint(Graphics g) {
+		
+	}
+
+}
